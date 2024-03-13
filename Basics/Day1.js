d13047172896e8d0a4107987b0698c8d6045f2cc
Astronaut004 <92384896+Astronaut004@users.noreply.github.com>
2024-03-13 08:15:22 +0000

    Day 1 change

---
+++ Basics/Day1.js
@@ -0,0 +1,23 @@
+// console.log("Aryan Chauhan");
+
+const code = 67;
+let name = "Aryan";
+var surname = "Chauhan";
+UserID = 4;
+
+console.log(code);
+console.log(name);
+console.log(surname);
+console.log(UserID);
+
+console.table([code,name,surname,UserID]);
+
+/*
+Always prefer let 
+Don't use var because it has some functional and block scope issues.
+*/
+
+
+//error cannot redeclare the same variable again with let
+// let name = "Abhi";
+// console.log(name);
