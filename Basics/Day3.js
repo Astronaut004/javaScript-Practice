ad25492b7f9d08296d9b2bae5a965ddaea65e058
Aryan Chauhan <ac4548817@gmail.comgit config --global user.name>
2024-03-15 00:56:51 +0530

    Day3 edit

--- Basics/Day3.js
+++ Basics/Day3.js
@@ -1,34 +1,35 @@
  const score = "33";
  const result = "33A";
 
- console.log(score);
-//  console.log(typeof(score));
- console.log(typeof(result));
+//  console.log(score);
+// //  console.log(typeof(score));
+//  console.log(typeof(result));
 
  let variableToNumber = Number(score);
  let variable = Number(result);
 
 //  console.log(variableToNumber);
 //  console.log(typeof(variableToNumber));
- console.log(variable); // it will give a output NaN
- console.log(typeof(variable));
+//  console.log(variable); // it will give a output NaN
+//  console.log(typeof(variable));
 
  /* one more example*/
-console.log("Example wih null:");
- const a = null;
- let b = Number(a);
- console.log(typeof(b));
- console.log(b);
+
+// console.log("Example wih null:");
+//  const a = null;
+//  let b = Number(a);
+//  console.log(typeof(b));
+//  console.log(b);
 
 
  /* one more example*/
  /* ek baar review krna pdega kyuki undefined me value NaN aati hai but output 0 aa rha hai
  */
-console.log("Example wih undefined:");
- const c = undefined;
- let d = Number(c);
- console.log(typeof(d));
- console.log(d);
+// console.log("Example wih undefined:");
+//  const c = undefined;
+//  let d = Number(c);
+//  console.log(typeof(d));
+//  console.log(d);
 
  /*
  Notes
@@ -48,7 +49,26 @@ console.log("Example wih undefined:");
             33 => "33"
  */
 
-console.log("From boolean to string: =>");
-const u = 0;
-let f = String(u);
-console.log(f);
+// console.log("From boolean to string: =>");
+// const u = 0;
+// let f = String(u);
+// console.log(f);
+
+
+
+// ***********************************************************operations**************************************************************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 3 + "4");
+// console.log("4" + 3 + 3);
+// console.log("4" + 8);
+// console.log(2 + "4");
+console.log(null + "4");
+console.log("4" + undefined);
+console.log(NaN + "4");
+console.log("4" + false);
+
+// string se phle conversion hoga but string ke baad string ki consider hoga
